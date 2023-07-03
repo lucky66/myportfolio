99c65e0855d43381658cd0ebbe44bd53c40dd93c
lucky66 <luckychukwu66@gmail.com>
2023-07-03 08:49:00 -0700

    first push

---
+++ script.js
@@ -0,0 +1,27 @@
+// function showContent(contentId) {
+//   // Hide all content divs
+//   var contents = document.getElementsByClassName('content');
+//   for (var i = 0; i < contents.length; i++) {
+//     contents[i].style.display = 'none';
+//   }
+
+//   // Show the selected content div
+//   var selectedContent = document.getElementById(contentId);
+//   if (selectedContent) {
+//     selectedContent.style.display = 'block';
+//   }
+// }
+
+function showContent(contentId) {
+  var contents = document.getElementsByClassName('content');
+  for (var i = 0; i < contents.length; i++) {
+    contents[i].style.display = 'none';
+  }
+
+  //show selected content
+  var showSelected = document.getElementById(contentId);
+  if (showSelected) {
+    showSelected.style.display = 'block';
+  }
+};
+
